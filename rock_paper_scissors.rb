5c6ef4f12bc022f86371d2571fdbe2a7780fd7f8
Dylan Wolff <dylanwolff@gmail.com>
2015-05-07 16:54:58 +1000

    Initial basic working implementation

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,3 +1,27 @@
+# Rock, Paper, Scissors
 
+CHOICES = { 'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors' }
 
 puts 'Play Rock, Paper, Scissors!'
+
+loop do
+  # player chooses move
+  begin
+    puts 'Pick one: (p, r, s ):'
+    player_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
+
+  # computer chooses move
+  computer_choice = CHOICES.keys.sample
+
+  if player_choice == computer_choice
+    puts "It's a tie!"
+  # player wins
+  elsif (player_choice == 'p' && computer_choice == 'r') ||
+        (player_choice == 'r' && computer_choice == 's') ||
+        (player_choice == 's' && computer_choice == 'p')
+    puts 'You won!'
+  else
+    puts 'Computer won!'
+  end
+end
