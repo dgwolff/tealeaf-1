224b7c8e81bd6cf5d6d0d34c8db200cb7addb184
Dylan Wolff <dylanwolff@gmail.com>
2015-05-07 20:54:52 +1000

    Initial rough out of logic

---
+++ blackjack.rb
@@ -0,0 +1,14 @@
+# Blackjack
+
+# Player is dealt two cards to start game
+# Dealer is dealt two cards to start game
+# Player chooses to hit or stay
+#   If total of cards = 21, player wins
+#   If total of cards > 21, player busts
+#   if total of cards < 21, player can hit again or stay
+#   If player hits, repeat above
+#   If player stays, player's total is saved and turn moves to the dealer
+# Dealer must continue to hit until total is >= 17
+#   If dealer total > 21, player wins
+#   If dealer total = 21, dealer wins
+#   If dealer total = 17-20, higher total hand wins
