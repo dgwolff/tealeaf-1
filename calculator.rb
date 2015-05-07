e0c9695728f5534a7e23cbef588565ac5fd7b293
Dylan Wolff <dylanwolff@gmail.com>
2015-05-07 14:47:41 +1000

    Add is_i? method and loops to allow only numeric values for num1 & num2

--- calculator.rb
+++ calculator.rb
@@ -1,22 +1,44 @@
 # Simple calculator
 
+# Initialise variables
+num1 = []
+num2 = []
+operator = []
+
+# Check whether user input is numeric
+class String
+  def is_i?
+    /\A[-+]?\d+\z/ === self
+  end
+end
+
+# Print defined text
 def say(msg)
   puts "=> #{msg}"
 end
 
+# Print result
 def say_result(result)
   puts "=> The result is #{result}"
 end
 
+# Begin program
 loop do
-  say "Welcome to the simple calculator!"
-  say "What's the first number?"
-  num1 = gets.chomp # modify to only allow numeric input
-
-  say "What's the second number?"
-  num2 = gets.chomp # as above
-
-  say "Choose an operation:"
+  say 'Welcome to the simple calculator!'
+  # Get num1 from user, permit only numeric input
+  loop do
+    say "What's the first number?"
+    num1 = gets.chomp
+    break if num1.is_i?
+  end
+  # Get num2 from user, permit only numeric input
+  loop do
+    say "What's the second number?"
+    num2 = gets.chomp
+    break if num2.is_i?
+  end
+  # Get selection from user, permit only numeric input from 1-4
+  say 'Choose an operation:'
   say '1) add 2) subtract 3) multiply 4) divide'
   operator = gets.chomp
 
@@ -37,7 +59,7 @@ loop do
       say_result(result)
     end
   end
-
+  # Run again?
   say 'Would you like to calculate again (Yes/No)?'
   calculate_again = gets.chomp.downcase
   break if calculate_again != 'y' && calculate_again != 'yes'
