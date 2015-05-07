7ddfb295d2a7656453f42c4ca16df0e42e495492
Dylan Wolff <dylanwolff@gmail.com>
2015-05-07 20:54:26 +1000

    Remove superfluous comments

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -16,18 +16,15 @@ def display_winning_message(winning_choice)
 end
 
 loop do
-  # player chooses move
   begin
     puts 'Pick one: (r, p, s):'
     player_choice = gets.chomp.downcase
   end until CHOICES.keys.include?(player_choice)
 
-  # computer chooses move
   computer_choice = CHOICES.keys.sample
 
   if player_choice == computer_choice
     puts "It's a tie!"
-  # player wins
   elsif (player_choice == 'p' && computer_choice == 'r') ||
         (player_choice == 's' && computer_choice == 'p') ||
         (player_choice == 'r' && computer_choice == 's')
