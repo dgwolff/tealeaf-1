476ca0e15e80bb1c8266a28805ee6fdf31440c19
Dylan Wolff <dylanwolff@gmail.com>
2015-05-07 17:04:34 +1000

    Complete build

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -4,10 +4,21 @@ CHOICES = { 'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors' }
 
 puts 'Play Rock, Paper, Scissors!'
 
+def display_winning_message(winning_choice)
+  case winning_choice
+  when 'r'
+    puts 'Rock smashes Scissors'
+  when 'p'
+    puts 'Paper wraps Rock'
+  when 's'
+    puts 'Scissors cuts Paper'
+  end
+end
+
 loop do
   # player chooses move
   begin
-    puts 'Pick one: (p, r, s ):'
+    puts 'Pick one: (r, p, s):'
     player_choice = gets.chomp.downcase
   end until CHOICES.keys.include?(player_choice)
 
@@ -18,10 +29,17 @@ loop do
     puts "It's a tie!"
   # player wins
   elsif (player_choice == 'p' && computer_choice == 'r') ||
-        (player_choice == 'r' && computer_choice == 's') ||
-        (player_choice == 's' && computer_choice == 'p')
+        (player_choice == 's' && computer_choice == 'p') ||
+        (player_choice == 'r' && computer_choice == 's')
+    display_winning_message(player_choice)
     puts 'You won!'
   else
+    display_winning_message(computer_choice)
     puts 'Computer won!'
   end
+
+  puts 'Play again? (y/n)'
+  break if gets.chomp.downcase != 'y'
 end
+
+puts 'Good bye!'
