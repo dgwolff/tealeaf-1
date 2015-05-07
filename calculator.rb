1a5657ac967e3bcaacc2d24527f4e55bd44ce08b
Dylan Wolff <dylanwolff@gmail.com>
2015-05-07 11:17:01 +1000

    Add say_result method

--- calculator.rb
+++ calculator.rb
@@ -1,36 +1,40 @@
 # Simple calculator
 
-require 'pry'
-
 def say(msg)
   puts "=> #{msg}"
 end
 
+def say_result(result)
+  puts "=> The result is #{result}"
+end
+
 loop do
+  say "Welcome to the simple calculator!"
   say "What's the first number?"
-  num1 = gets.chomp
+  num1 = gets.chomp # modify to only allow numeric input
 
   say "What's the second number?"
-  num2 = gets.chomp
+  num2 = gets.chomp # as above
 
+  say "Choose an operation:"
   say '1) add 2) subtract 3) multiply 4) divide'
   operator = gets.chomp
 
   if operator == '1'
     result = num1.to_i + num2.to_i
-    say "Result is #{result}"
+    say_result(result)
   elsif operator == '2'
     result = num1.to_i - num2.to_i
-    say "Result is #{result}"
+    say_result(result)
   elsif operator == '3'
     result = num1.to_i * num2.to_i
-    say "Result is #{result}"
+    say_result(result)
   else
     if num2 == '0'
       say 'Error! Cannot divide by 0!'
     else
       result = num1.to_f / num2.to_f
-      say "Result is #{result}"
+      say_result(result)
     end
   end
 
