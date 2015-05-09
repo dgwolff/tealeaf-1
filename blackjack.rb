da3bd4522b6c6014438ac9ae7381f0a6c0e9e13f
Dylan Wolff <dylanwolff@gmail.com>
2015-05-09 16:28:05 +1000

    Initial rough out, add calculate_total method

--- blackjack.rb
+++ blackjack.rb
@@ -12,3 +12,77 @@
 #   If dealer total > 21, player wins
 #   If dealer total = 21, dealer wins
 #   If dealer total = 17-20, higher total hand wins
+
+def say(msg)
+  puts "=> #{msg}"
+end
+
+def calculate_total(cards)
+  arr = cards.map { |e| e[0] }
+
+  total = 0
+  arr.each do |value|
+    if value == "Ace"
+      total += 11
+    elsif value.to_i == 0
+      total += 10
+    else
+      total += value.to_i
+    end
+  end
+
+  arr.select { |e| e == "Ace" }.count.times do
+    total -= 10 if total > 21
+  end
+
+  total
+end
+
+say "Welcome to Blackjack!"
+
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+
+deck = cards.product(suits)
+deck.shuffle!
+
+# Deal cards
+
+player_cards = []
+dealer_cards = []
+
+player_cards << deck.pop
+dealer_cards << deck.pop
+player_cards << deck.pop
+dealer_cards << deck.pop
+
+player_total = calculate_total(player_cards)
+dealer_total = calculate_total(dealer_cards)
+
+# Show cards
+
+say "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}"
+say "Dealer has: #{dealer_cards[0]} and #{dealer_cards[1]} for a total of #{dealer_total}"
+puts ""
+say "What would you like to do? 1) Hit 2) Stay"
+hit_or_stay = gets.chomp
+
+case hit_or_stay
+when "1"
+  player_cards << deck.pop
+  p player_cards
+when "2"
+  say "You chose to stay"
+end
+
+
+
+
+
+
+
+
+
+
+
+
