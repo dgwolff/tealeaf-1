4232f94856e257824e360254eb0c96bd7ba1b732
Dylan Wolff <dylanwolff@gmail.com>
2015-05-18 13:46:37 +1000

    Initial rough-out of OO Blackjack

---
+++ week_2/oo_blackjack.rb
@@ -0,0 +1,67 @@
+# Object-Oriented Blackjack
+
+# 1. Have detailed requirements or specifications in written form
+# 2. Extract major nouns -> classes
+# 3. Extract major verbs -> instance methods
+# 4. Group instance methods into classes
+
+class Card
+  attr_accessor :suit, :value
+
+  def initialize(s, v)
+    @suit = s
+    @value = v
+  end
+
+  def to_s
+    "The card is #{value} of #{suit}"
+  end
+end
+
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    ['H', 'D', 'S', 'C'].each do |suit|
+      ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].each do |face_value|
+        @cards << Card.new(suit, face_value)
+      end
+    end
+    scramble!
+  end
+
+  def scramble!
+    cards.shuffle!
+  end
+
+  def deal
+    cards.pop
+  end
+end
+
+class Player
+end
+
+class Dealer
+end
+
+class Blackjack
+  attr_accessor :player, :dealer, :deck
+
+  def initialize
+    @player = Player.new
+    @dealer = Dealer.new
+    @deck = Deck.new
+  end
+
+  def run
+    deal_cards
+    show_flop
+    player_turn
+    dealer_turn
+    who_won?
+  end
+end
+
+Blackjack.new.run
