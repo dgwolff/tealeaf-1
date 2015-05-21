84f786140c74c575e8cf6f276f8b4614e2c837a0
Dylan Wolff <dylanwolff@gmail.com>
2015-05-21 20:14:43 +0800

    Complete OO Blackjack stage 2

--- week_2/oo_blackjack.rb
+++ week_2/oo_blackjack.rb
@@ -12,9 +12,49 @@ class Card
     @suit = s
     @face_value = fv
   end
+
+  def pretty_output
+    puts "The #{face_value} of #{find_suit}"
+  end
+
+  def to_s
+    pretty_output
+  end
+
+  def find_suit
+    case suit
+    when "H" then "Hearts"
+    when "D" then "Diamonds"
+    when "S" then "Spades"
+    when "C" then "Clubs"
+    end
+  end
 end
 
 class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    ["H", "D", "S", "C"].each do |suit|
+      ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"].each do |face_value|
+        @cards << Card.new(suit, face_value)
+      end
+    end
+    scramble!
+  end
+
+  def scramble!
+    cards.shuffle!
+  end
+
+  def deal_one
+    cards.pop
+  end
+
+  def size
+    cards.size
+  end
 end
 
 class Player
@@ -26,7 +66,6 @@ end
 class Hand
 end
 
-c1 = Card.new
-
+deck = Deck.new
 
 
